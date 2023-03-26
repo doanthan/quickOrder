52e5da6dab93b61ecceabbd1d1cd4182cb33a731
Doan Than <doanthan@gmail.com>
2023-03-26 12:06:35 +1100

    added test pages

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,24 @@ import OrderSuccessScreen from "./screens/OrderSuccessScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegistrationScreen from "./screens/RegistrationScreen";
 import AdminConsoleScreen from "./screens/AdminScreen";
+import MTest1 from "./screens/mtest1";
+import MTest2 from "./screens/mtest2";
+import MTest3 from "./screens/mtest3";
+import MTest4 from "./screens/mtest4";
+import MTest5 from "./screens/mtest5";
+import MTest6 from "./screens/mtest6";
+import MTest7 from "./screens/mtest7";
+import MTest8 from "./screens/mtest8";
+import MTest9 from "./screens/mtest9";
+import DTest1 from "./screens/dtest1";
+import DTest2 from "./screens/dtest2";
+import DTest3 from "./screens/dtest3";
+import DTest4 from "./screens/dtest4";
+import DTest5 from "./screens/dtest5";
+import DTest6 from "./screens/dtest6";
+import DTest7 from "./screens/dtest7";
+import DTest8 from "./screens/dtest8";
+import DTest9 from "./screens/dtest9";
 
 function App() {
   return (
@@ -31,6 +49,24 @@ function App() {
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/registration" element={<RegistrationScreen />} />
             <Route path="/admin-console" element={<AdminConsoleScreen />} />
+            <Route path="/mtest1" element={<MTest1 />} />
+            <Route path="/mtest2" element={<MTest2 />} />
+            <Route path="/mtest3" element={<MTest3 />} />
+            <Route path="/mtest4" element={<MTest4 />} />
+            <Route path="/mtest5" element={<MTest5 />} />
+            <Route path="/mtest6" element={<MTest6 />} />
+            <Route path="/mtest7" element={<MTest7 />} />
+            <Route path="/mtest8" element={<MTest8 />} />
+            <Route path="/mtest9" element={<MTest9 />} />
+            <Route path="/dtest1" element={<DTest1 />} />
+            <Route path="/dtest2" element={<DTest2 />} />
+            <Route path="/dtest3" element={<DTest3 />} />
+            <Route path="/dtest4" element={<DTest4 />} />
+            <Route path="/dtest5" element={<DTest5 />} />
+            <Route path="/dtest6" element={<DTest6 />} />
+            <Route path="/dtest7" element={<DTest7 />} />
+            <Route path="/dtest8" element={<DTest8 />} />
+            <Route path="/dtest9" element={<DTest9 />} />
           </Routes>
         </main>
         <Footer />
---
+++ client/src/screens/mtest1.js
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+//TODO: redefine password length
+const MTest1 = () => {
+  return (
+    <Container
+      maxW="lg"
+      py={{ base: "12", md: "24" }}
+      px={{ base: "0", md: "8" }}
+      minH="4xl"
+    >
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+            <Heading>Thiis is a test page</Heading>
+            <HStack spacing="1" justify="center">
+              <Text color="muted">Don't have an account ?</Text>
+              <Button variant="link" colorScheme="orange">
+                Martin test
+              </Button>
+            </HStack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+};
+
+export default MTest1;
